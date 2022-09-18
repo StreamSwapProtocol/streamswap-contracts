1e4829803acb916d729bf5e3e2e56d7dba218644
Orkun Külçe <orkunkl@users.noreply.github.com>
2022-09-18 13:16:56 +0300

    Move cw20-reward code

--- src/error.rs
+++ src/error.rs
@@ -1,16 +1,32 @@
-use cosmwasm_std::StdError;
+use cosmwasm_std::{OverflowError, StdError, Uint128};
 use thiserror::Error;
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug, PartialEq)]
 pub enum ContractError {
     #[error("{0}")]
     Std(#[from] StdError),
 
+    #[error("{0}")]
+    Overflow(#[from] OverflowError),
+
+    #[error("No rewards accrued")]
+    NoRewards {},
+
     #[error("Unauthorized")]
     Unauthorized {},
 
-    #[error("Custom Error val: {val:?}")]
-    CustomError { val: String },
-    // Add any other custom errors you like here.
-    // Look at https://docs.rs/thiserror/1.0.21/thiserror/ for details.
+    #[error("Do not send native funds")]
+    DoNotSendFunds {},
+
+    #[error("Amount required")]
+    AmountRequired {},
+
+    #[error("Decrease amount exceeds user balance: {0}")]
+    DecreaseAmountExceeds(Uint128),
+
+    #[error("Wait for the unbonding")]
+    WaitUnbonding {},
+
+    #[error("No bond")]
+    NoBond {},
 }
