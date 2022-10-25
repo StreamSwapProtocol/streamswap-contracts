a402928df3d1ef1aa3ec55d159b106e8bfdd0b98
Orkun Külçe <orkunkl@users.noreply.github.com>
2022-10-25 18:30:10 +0300

    Add schema

---
+++ bin/schema.rs
@@ -0,0 +1,11 @@
+use cosmwasm_schema::write_api;
+
+use cw_streamed::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+fn main() {
+    write_api! {
+        instantiate: InstantiateMsg,
+        execute: ExecuteMsg,
+        query: QueryMsg,
+    }
+}
