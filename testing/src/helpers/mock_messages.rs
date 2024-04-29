a8f8f0196deff31d796e6c5e25e09514e1034019
Ninjatosba <adenizcorlu@gmail.com>
2024-04-29 22:38:29 +0300

    add create stream message

---
+++ testing/src/helpers/mock_messages.rs
@@ -0,0 +1,48 @@
+use cosmwasm_std::{Coin, Decimal};
+use cw_streamswap_factory::msg::{CreateStreamMsg, InstantiateMsg as FactoryInstantiateMsg};
+
+use super::setup::TestAccounts;
+
+pub fn get_factory_inst_msg(
+    stream_swap_code_id: u64,
+    test_accounts: TestAccounts,
+) -> FactoryInstantiateMsg {
+    FactoryInstantiateMsg {
+        stream_swap_code_id,
+        protocol_admin: Some(test_accounts.admin.to_string()),
+        fee_collector: Some(test_accounts.admin.to_string()),
+        stream_creation_fee: Coin {
+            denom: "fee_token".to_string(),
+            amount: 100u128.into(),
+        },
+        exit_fee_percent: Decimal::percent(1),
+        accepted_in_denoms: vec!["in_denom".to_string()],
+        min_stream_blocks: 10,
+        min_blocks_until_start_block: 10,
+    }
+}
+
+pub fn get_create_stream_msg(
+    name: &str,
+    treasury: &str,
+    out_asset_denom: &str,
+    in_denom: &str,
+    start_block: u64,
+    end_block: u64,
+    threshold: Option<u128>,
+) -> CreateStreamMsg {
+    CreateStreamMsg {
+        treasury: treasury.to_string(),
+        stream_admin: treasury.to_string(),
+        name: name.to_string(),
+        url: None,
+        out_asset: Coin {
+            denom: out_asset_denom.to_string(),
+            amount: 100u128.into(),
+        },
+        in_denom: in_denom.to_string(),
+        start_block,
+        end_block,
+        threshold: threshold.map(|t| t.into()),
+    }
+}
--- testing/src/helpers/mod.rs
+++ testing/src/helpers/mod.rs
@@ -1,1 +1,2 @@
+mod mock_messages;
 mod setup;
