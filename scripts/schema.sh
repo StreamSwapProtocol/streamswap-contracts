69db4d1ebdc59543513ffad7693347f3e5316827
Ninjatosba <adenizcorlu@gmail.com>
2024-07-11 14:17:54 +0300

    remove script prints

--- scripts/schema.sh
+++ scripts/schema.sh
@@ -3,14 +3,8 @@
 # Define the main directory path
 MAIN_DIR=$(dirname "$(realpath "$0")")/..
 
-# Print out the main directory path for debugging
-echo "Main directory: $MAIN_DIR"
-
 CONTRACTS_DIR="$MAIN_DIR/contracts"
 TS_DIR="$MAIN_DIR/ts"
-
-# Print out the contracts directory path for debugging
-echo "Contracts directory: $CONTRACTS_DIR"
 
 # Iterate over contracts
 for d in "$CONTRACTS_DIR"/*; do
@@ -21,9 +15,6 @@ for d in "$CONTRACTS_DIR"/*; do
     cd "$MAIN_DIR" || exit
   fi
 done
-
-# Print out the ts directory path for debugging
-echo "TS directory: $TS_DIR"
 
 # go main directory/ts
 cd "$TS_DIR" || exit
