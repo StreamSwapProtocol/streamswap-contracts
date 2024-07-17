761a840e7dc80f81a0d6f9421bf92ad77953344a
Ninjatosba <adenizcorlu@gmail.com>
2024-07-17 22:55:58 +0300

    change status structure

--- packages/types/src/stream/stream.rs
+++ packages/types/src/stream/stream.rs
@@ -30,14 +30,10 @@ pub struct Stream {
     pub spent_in: Uint128,
     /// Total number of shares minted.
     pub shares: Uint128,
-    /// Start block when the token emission starts. in nanos.
-    pub start_time: Timestamp,
-    /// End block when the token emission ends.
-    pub end_time: Timestamp,
     /// Price at when latest distribution is triggered.
     pub current_streamed_price: Decimal,
     /// Status of the stream. Can be `Waiting`, `Active`, `Finalized`, `Paused` or `Canceled` for kill switch.
-    pub status: Status,
+    pub status: StreamStatus,
     /// Create Pool message
     pub create_pool: Option<CreatePool>,
     /// Vesting configuration
@@ -46,11 +42,53 @@ pub struct Stream {
 
 #[cw_serde]
 pub enum Status {
-    /// Waiting for start date
     Waiting,
+    Boothstraping,
     Active,
+    Ended,
     Finalized,
     Cancelled,
+}
+
+#[cw_serde]
+pub struct StreamStatus {
+    pub status: Status,
+    pub bootstraping_start_time: Timestamp,
+    pub start_time: Timestamp,
+    pub end_time: Timestamp,
+}
+
+impl StreamStatus {
+    pub fn new(
+        bootstraping_start_time: Timestamp,
+        start_time: Timestamp,
+        end_time: Timestamp,
+    ) -> Self {
+        StreamStatus {
+            status: Status::Waiting,
+            bootstraping_start_time,
+            start_time,
+            end_time,
+        }
+    }
+    pub fn update_status(&mut self, now: Timestamp) {
+        // If bootstraping time is not reached yet, keep the status as Waiting
+        // If bootstraping time is reached, change the status to Boothstraping
+        // If start time is reached, change the status to Active
+        // If end time is reached, change the status to Ended
+        if now < self.bootstraping_start_time {
+            self.status = Status::Waiting;
+        }
+        if now >= self.bootstraping_start_time && now < self.start_time {
+            self.status = Status::Boothstraping;
+        }
+        if now >= self.start_time && now < self.end_time {
+            self.status = Status::Active;
+        }
+        if now >= self.end_time {
+            self.status = Status::Ended;
+        }
+    }
 }
 
 impl Stream {
@@ -61,6 +99,7 @@ impl Stream {
         url: Option<String>,
         out_asset: Coin,
         in_denom: String,
+        boothstraping_start_time: Timestamp,
         start_time: Timestamp,
         end_time: Timestamp,
         last_updated: Timestamp,
@@ -74,8 +113,6 @@ impl Stream {
             url,
             dist_index: Decimal256::zero(),
             last_updated,
-            start_time,
-            end_time,
             out_asset: out_asset.clone(),
             out_remaining: out_asset.amount,
             in_denom,
@@ -83,7 +120,7 @@ impl Stream {
             spent_in: Uint128::zero(),
             shares: Uint128::zero(),
             current_streamed_price: Decimal::zero(),
-            status: Status::Waiting,
+            status: StreamStatus::new(boothstraping_start_time, start_time, end_time),
             create_pool,
             vesting,
         }
@@ -103,12 +140,7 @@ impl Stream {
         shares
     }
 
-    pub fn is_cancelled(&self) -> bool {
-        self.status == Status::Cancelled
-    }
-
-    // TODO: rename
-    pub fn is_killswitch_active(&self) -> bool {
-        self.status == Status::Cancelled
+    pub fn update_status(&mut self, now: Timestamp) {
+        self.status.update_status(now);
     }
 }
