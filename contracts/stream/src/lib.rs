3c2757f1237c0f9fe1efd531f69dc841678f9e1e
Orkun Külçe <orkunkl@users.noreply.github.com>
2024-08-20 14:11:56 +0300

    Rename killswitch to circuit ops (#81)
    * Rename killswitch to circuit ops
    
    * Fix fmt

--- contracts/stream/src/lib.rs
+++ contracts/stream/src/lib.rs
@@ -1,10 +1,10 @@
 extern crate core;
 
 pub use crate::error::ContractError;
+mod circuit_ops;
 pub mod contract;
 mod error;
 mod helpers;
-mod killswitch;
 mod pool;
 pub mod state;
 pub mod stream;
